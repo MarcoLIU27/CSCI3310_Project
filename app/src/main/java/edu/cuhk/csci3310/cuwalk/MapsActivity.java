134b2372602b2b18f47c6deccf055867a139352a
williw23 <daiweican@gmail.com>
2023-05-15 10:11:46 +0800

    基本的运动记录功能，可在其他界面同时后台运行

--- app/src/main/java/edu/cuhk/csci3310/cuwalk/MapsActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/cuwalk/MapsActivity.java
@@ -29,6 +29,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
